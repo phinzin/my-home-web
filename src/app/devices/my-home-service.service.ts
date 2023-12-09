eefcbba2f6f032078cc57a9809bed40bd4f704f5
phinzin <phinzin2@gmail.com>
2023-12-09 10:50:38 +0000

    change api

--- src/app/devices/my-home-service.service.ts
+++ src/app/devices/my-home-service.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class MyHomeServiceService {
-  private apiUrl = 'http://my-management-api-ix-chart.ix-my-management-api.svc.cluster.local:9081/api/my-home';
-
+  // private apiUrl = 'http://my-management-api-ix-chart.ix-my-management-api.svc.cluster.local:9081/api/my-home';
+  private apiUrl = 'http://192.168.1.46:9081/api/my-home'
   constructor(private http: HttpClient) {}
   getData(): Observable<any> {
     return this.http.get(`${this.apiUrl}/all`);
