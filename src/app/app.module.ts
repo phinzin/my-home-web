7c88fb2881d52d173eea6fc50317d5d43b83b660
phinzin <phinzin2@gmail.com>
2023-12-10 09:09:19 +0000

    change name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,15 @@ import { MatNativeDateModule } from '@angular/material/core';
 import {MatTableModule} from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
 import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 @NgModule({
@@ -32,7 +41,23 @@ import { MatPaginatorModule} from '@angular/material/paginator';
     MatTableModule,
     MatTableModule,
     MatPaginatorModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { MyHomeServiceService } from './my-home-service.service';
 
 
@@ -9,41 +9,33 @@ import { MyHomeServiceService } from './my-home-service.service';
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss'],
-  
+
 })
 
-export class DevicesComponent implements AfterViewInit  {
-  displayedColumns: string[] = ['name', 'recentStage','nextStage','note'];
+export class DevicesComponent implements AfterViewInit {
+  displayedColumns: string[] = ['name', 'recentStage', 'nextStage', 'note'];
   dataSource = new MatTableDataSource<MyHomeItem>();
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
-
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    console.log(2)
     this.myHomeService.getData().subscribe((data) => {
-      console.log(data);
-      console.log(1)
       this.dataSource.data = data;
     });
   }
 
-  constructor(private myHomeService: MyHomeServiceService){}
-  
+  constructor(private myHomeService: MyHomeServiceService) {
+  }
+
 }
+
 export interface MyHomeItem {
-  partitionKey:string,
-  id:string,
-  name:string,
-  executedDate:string,
-  nextStage:string,
-  remark:string
+  partitionKey: string,
+  id: string,
+  name: string,
+  executedDate: string,
+  nextStage: string,
+  remark: string
 }
 
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {position: 1, name: 'Lõi Lọc - Bình Chánh', recentStage: '12/09/2023',nextStage:'12/12/2023',note:'đã thay 3 lõi'},
-//   {position: 2, name: 'Vệ sinh máy lạnh - Phú Định', recentStage: '12/10/2023',nextStage:'12/04/2024', note:''},
-//   {position: 3, name: 'Chích ngừa', recentStage: '11/11/2023',nextStage:'12/12/2023', note:''}
-
-// ];
